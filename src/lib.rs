18b44d39084360e2ba964292f6d6de5c6abe18fb
Thomas Koehler <thomas.koehler@thok.eu>
2025-09-18 17:30:56 +0200

    some rules

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,4 @@ mod lang;
 pub use lang::{Rise, RiseAnalysis};
 pub type RiseEGraph = EGraph<Rise, RiseAnalysis>;
 pub type RiseExpr = RecExpr<Rise>;
+pub type RiseRewrite = Rewrite<Rise, RiseAnalysis>;
